4127a154ea3c7633e3149e474df2696b1c49e250
Andrew McClenaghan <andrew.mcclenaghan@gmail.com>
2017-04-25 10:13:04 +1000

    Update to VS2017 csproj

--- src/GreenPipes/CorePolyfills.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace System
-{
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate)]
-    internal class SerializableAttribute : Attribute
-    {
-    }
-}
